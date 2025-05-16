a5b9d5d654ea82097db850cd64bc65f6e481a50e
Chesyon <55322011+Chesyon@users.noreply.github.com>
2025-05-16 18:23:58 -0400

    Rewrote name autofill to use a buffer
    Turns out writing a null byte to the 11th character only worked in the short term, and it becomes really inconvenient once you start using the slot name for other stuff. So now we just copy the first 10 characters into a buffer, and pass the buffer to the name menu. This way, the slot name is no longer being modified at runtimee, fixing the underlying issues with random starters not being consistent. We're also using all 16 characters for hero randomization now that we can do so safely!
    ov36: 2721792
    hintables: 3299328

--- src/name_autofill.s
+++ src/name_autofill.s
@@ -1,6 +1,14 @@
 .align 4
-AutofillName: 
-    ldr  r1,=slotData // both sets up for truncation and sets the pointer for the buffer
-    strb r0,[r1,#10]  // truncate string by writing a null byte at the 11th character. r0 already contains 0 from the original code.
-    mov  r2,#0        // original code
-    bx   lr           // return to original code
+AutofillName:
+    push  {r14}
+    ldr   r0,=NameBuffer // We want to copy the name to the buffer
+    ldr   r1,=slotData   // We want to get the name from slot data
+    mov   r2,#10         // We want to copy ten characters
+    bl    strncpy        
+    mov   r1,r0          // Set the name menu to read from the buffer (r0 is the same at the end of strncpy, so it's still a pointer to NameBuffer)
+    mov   r0,#0x0        // r0 was 0 in original code
+    mov   r2,#0          // original code
+    pop   {r15}          // return to original code
+NameBuffer:
+    .fill 0xB,0x0        // buffer for the name, so we don't overwrite the original info. last byte is reserved to be null
+.pool
