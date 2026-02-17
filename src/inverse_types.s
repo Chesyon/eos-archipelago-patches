92809b4f4fa02785a05d386bb67ea30c36ff8613
Chesyon <55322011+Chesyon@users.noreply.github.com>
2026-02-17 17:03:06 -0500

    oops

--- src/inverse_types.s
+++ src/inverse_types.s
@@ -15,11 +15,11 @@ IsTypeIneffectiveAgainstGhostFlip:
     bx   lr
 
 GetTypeMatchupFlip:
-  ldr   r1,=CUSTOM_SAVE_AREA
-  ldrb  r1,[r1,#0x2C0] // or some other valid immediate; i just picked the first free byte in the save area.
-  cmp   r1,#0
-  popeq {r3-r7,pc}
-  cmp   r0,#2    // eq: neutral -> neutral, no change needed
-  addlt r0,r0,#2 // immune -> neutral, NVE -> SE
-  subgt r0,r0,#2 // SE -> NVE
-  pop   {r3-r7,pc}
+    ldr   r1,=CUSTOM_SAVE_AREA
+    ldrb  r1,[r1,#0x2C0] // or some other valid immediate; i just picked the first free byte in the save area.
+    cmp   r1,#0
+    popeq {r3-r7,pc}
+    cmp   r0,#2    // eq: neutral -> neutral, no change needed
+    movlt r0, #3 // immune or NVE to SE
+    movgt r0, #1 // SE to NVE
+    pop   {r3-r7,pc}
