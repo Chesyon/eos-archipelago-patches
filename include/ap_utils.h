eec8b6c600545b34f3b048483a48368b43548b0d
Joshua Pang <32785974+Jawshoeuh@users.noreply.github.com>
2026-02-25 02:30:49 -0800

    Documentation For ap_utils.

--- include/ap_utils.h
+++ include/ap_utils.h
@@ -12,18 +12,95 @@ enum dungeon_check_type {
     DCT_RULE = 3
 };
 
-enum dungeon_check_type GetDungeonCheckType(short dunId);
+/* GetDungeonCheckType
+    Returns the type of missions checks that this dungeon has.
 
+    dungeon_id: dungeon to get the type of mission checks
+    return: dungeon_check_type */
+enum dungeon_check_type GetDungeonCheckType(short dungeon_id);
+
+/* GetSubXBit
+    Returns the status of the xth bit in the corresponding
+    VAR_SCENARIO_SUBX scripting variable there are 8
+    VAR_SCENARIO_SUBX variables with two indices and each
+    indices stores 1 byte (or 8 bits). So, if we wanted to
+    obtain the 33rd bit, we would get it by checking
+    VAR_SCENARIO_SUB3[0] & 0b00000010.
+
+
+    bit: 0-127 bit to check
+    return: value of the corresponding bit */
 bool GetSubXBit(int bit);
 
+/* GetRemainingDungeonMissionChecks
+    Calculates how many missions checks remain of a certain
+    type for a given dungeon.
+
+    dungeon: dungeon to check remaining mission count for
+    bool: whether to check outlaw missions instead of normal missions
+    return: number of uncompleted mission checks */
 int GetRemainingDungeonMissionChecks(enum dungeon_id dungeon, bool outlaw);
 
+/* IsDarkraiGoal
+    return: Is Goal Darkrai? */
 bool IsDarkraiGoal();
 
+/* IsDarkraiGoal
+    return: If long locations like Zero Isle North and 9999999 money
+            are enabled. */
 bool AreLongLocationsOn();
 
+/* GetDungeonModeWithSevenTreasureCheck
+    If the dungeon is not a Seven Treasure dungeon, just
+    returns the DMODE. However, if it a Seven Treasure dungeon
+    also check the corresponding SUBX bit for sending the
+    Seven Treasure mission acquired check. This check has been
+    added to ensure that the Seven Treasure missions can still
+    be obtained in the Cafe even when the dungeon has already
+    been unlocked.
+
+    dungeon_id: the dungeon to get the DMODE of
+    return: DMODE of the dungeon */
 enum dungeon_mode GetDungeonModeWithSevenTreasureCheck(enum dungeon_id dungeon_id);
 
+/* IsLookaLikeDungeonPlaceholder
+    To get look-alike dungeons to be near their corresponding
+    dungeon in the list at the crossroads requires renaming certain
+    dungeons that can't normally show up in the list. ie:
+    Beach Cave Pit AND setting their DMODE to
+    DMODE_OPEN_AND_REQUEST. Because of this, we need to add
+    some checks in places to not do stuff like generate missions
+    for DUNGEON_BEACH_CAVE_PIT and not put the dungeon in the
+    curated mission menu list.
+
+    dungeon_id: the dungeon to check
+    return: is a lookalike dungeon? */
 bool IsLookaLikeDungeonPlaceholder(enum dungeon_id dungeon_id);
 
-bool GetDebugRomFlagEnabled();
+/* GetDebugRomFlag
+    Checks if the debug cheat is active by checking if the
+    instruction has been changed.
+
+    return: debug cheat on? */
+bool GetDebugRomFlag();
+
+/* DrawVariableInfoInWindow
+    Draws the value for a variable into the window passed and
+    accounts for ALL types.
+    
+    idx: the window to draw in
+    x: x location to start drawing
+    y: y location to start drawing
+    max_width: max_width to use after x starting before new line
+    buffer: a buffer to temporarily write strings into
+    buffer_len: length of buffer
+    var: the variable to draw the info for */
+void DrawVariableInfoInWindow(
+    int idx,
+    int x,
+    int y,
+    int max_width,
+    char* buffer,
+    int buffer_len,
+    enum script_var_id var
+);
--- src/ap_utils.c
+++ src/ap_utils.c
@@ -8,34 +8,34 @@
 #define GOAL_DIALGA 1
 #define GOAL_DARKRAI 0
 
-enum dungeon_check_type GetDungeonCheckType(short dunId) {
+enum dungeon_check_type GetDungeonCheckType(short dungeon_id) {
     if (newApSettings.nums.goal == GOAL_DIALGA) {
-        if(dunId == DUNGEON_TEMPORAL_TOWER || dunId == DUNGEON_HIDDEN_LAND) {
+        if(dungeon_id == DUNGEON_TEMPORAL_TOWER || dungeon_id == DUNGEON_HIDDEN_LAND) {
             return DCT_OTHER;
         }
     }
     
-    if(dunId == DUNGEON_DARK_CRATER || dunId == DUNGEON_SKY_PEAK_SUMMIT) {
+    if(dungeon_id == DUNGEON_DARK_CRATER || dungeon_id == DUNGEON_SKY_PEAK_SUMMIT) {
         return DCT_OTHER;
     }
     
-    if(dunId >= DUNGEON_ICE_AEGIS_CAVE && dunId <= DUNGEON_REGIGIGAS_CHAMBER) {
+    if(dungeon_id >= DUNGEON_ICE_AEGIS_CAVE && dungeon_id <= DUNGEON_REGIGIGAS_CHAMBER) {
         return DCT_OTHER;
     }
     
-    if(dunId == DUNGEON_STAR_CAVE) {
+    if(dungeon_id == DUNGEON_STAR_CAVE) {
         return DCT_EARLY;
     }
     
-    if(DUNGEON_ZERO_ISLE_NORTH <= dunId && dunId <= DUNGEON_INFERNO_CAVE) {
+    if(DUNGEON_ZERO_ISLE_NORTH <= dungeon_id && dungeon_id <= DUNGEON_INFERNO_CAVE) {
         return DCT_RULE;
     }
     
-    if(dunId <= DUNGEON_BRINE_CAVE_PIT || (DUNGEON_SERENITY_RIVER <= dunId && dunId <= DUNGEON_ORAN_FOREST)) {
+    if(dungeon_id <= DUNGEON_BRINE_CAVE_PIT || (DUNGEON_SERENITY_RIVER <= dungeon_id && dungeon_id <= DUNGEON_ORAN_FOREST)) {
         return DCT_EARLY;
     }
     
-    if(dunId <= DUNGEON_DEEP_MYSTERY_JUNGLE || (DUNGEON_LAKE_AFAR <= dunId && dunId <= DUNGEON_SKY_PEAK_SUMMIT)) {
+    if(dungeon_id <= DUNGEON_DEEP_MYSTERY_JUNGLE || (DUNGEON_LAKE_AFAR <= dungeon_id && dungeon_id <= DUNGEON_SKY_PEAK_SUMMIT)) {
         return DCT_LATE;
     }
     
@@ -44,6 +44,10 @@ enum dungeon_check_type GetDungeonCheckType(short dunId) {
 
 // Convenience function to grab a bit from the SubX array.
 bool GetSubXBit(int bit) {
+    if(bit >= 128) {
+        return false;
+    }
+
     uint32_t temp = bit >> 3;
     uint32_t subXVal = LoadScriptVariableValueAtIndex(NULL, VAR_SCENARIO_SUB1 + (bit >> 4), temp & 0b1);
     uint32_t subXBitMask = 0b1 << (bit - (temp << 3));
@@ -122,7 +126,7 @@ bool IsLookaLikeDungeonPlaceholder(enum dungeon_id dungeon_id) {
 
 // Note: This works by checking the instruction in overlay11 being changed to
 // 'cmp r0,#0x1' specifically.
-bool __attribute__((naked)) GetDebugRomFlagEnabled() {
+bool __attribute__((naked)) GetDebugRomFlag() {
     asm("ldr r0,=ScriptingDebugInstructionAddr");
     asm("ldr r0,[r0,#0x0]");
     asm("ldr r1,=0xE3500001");
@@ -130,4 +134,56 @@ bool __attribute__((naked)) GetDebugRomFlagEnabled() {
     asm("moveq r0,#1");
     asm("movne r0,#0");
     asm("bx lr");
+}
+
+// Utility function to help us debug.
+void DrawDebugInfoInWindow(int idx, int x, int y, int max_width, char* buffer, int buffer_len, enum script_var_id var) {
+    struct preprocessor_args pre_args = {};
+    struct preprocessor_flags pre_flags = {};
+    struct script_var *variable_info = &(SCRIPT_VARS.vars[var]);
+    int width = GetStringWidth(variable_info->name);
+    DrawTextInWindow(idx, x, y, variable_info->name);
+    DrawTextInWindow(idx, x + width + 2, y, ":");
+    width+=8;
+    if (width >= max_width) {
+        width = 0;
+        y += 13;
+    }
+    for(int i = 0; i < variable_info->n_values; i++) {
+        switch(variable_info->type.val) {
+            case VARTYPE_NONE:;
+                return;
+            case VARTYPE_BIT:;
+                if(LoadScriptVariableValueAtIndex(NULL, var, i)){
+                    DrawTextInWindow(idx, x + width, y, "[M:S2]");
+                    width += 8;
+                } else {
+                    DrawTextInWindow(idx, x + width, y, "[M:S4]");
+                    width += 8;
+                }
+                break;
+            case VARTYPE_STRING:;
+                buffer[0] = LoadScriptVariableValueAtIndex(NULL, var, i);
+                buffer[1] = '\0';
+                width += GetStringWidth(buffer);
+                DrawTextInWindow(idx, x + width, y, buffer);
+                break;
+            case VARTYPE_UINT8:;
+            case VARTYPE_INT8:;
+            case VARTYPE_UINT16:;
+            case VARTYPE_INT16:;
+            case VARTYPE_UINT32:;
+            case VARTYPE_INT32:;
+            case VARTYPE_SPECIAL:;
+                pre_args.number_vals[0] = LoadScriptVariableValueAtIndex(NULL, var, i);
+                PreprocessString(buffer, buffer_len, (i > 0) ? ",[value:0]" : "[value:0]", pre_flags, &pre_args);
+                DrawTextInWindow(idx, x + width, y, buffer);
+                width += GetStringWidth(buffer);
+                break;
+        }
+        if(width >= max_width) {
+            width = 0;
+            y += 13;
+        }
+    }
 }
